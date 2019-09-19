b40f599c01a5e820ec9cf2e2206963769d65b672
卢宏政 <luhongzheng@changshafubao.com>
2019-09-19 09:31:28 +0800

    学习

--- src/components/tabbar/homeContainer.vue
+++ src/components/tabbar/homeContainer.vue
@@ -1,12 +1,61 @@
 <template>
   <div>
+    <mt-swipe>
+  <mt-swipe-item v-for="item in lunboList" :key="item.id">
+        <img :src="item.img" alt=""/>
+  </mt-swipe-item>
+</mt-swipe>
+
     <h1>HomeContainer</h1>
   </div>
 </template>
 
 <script>
+import {Toast} from 'mint-ui'
+export default{
+  data(){
+    return{
+      lunboList:[],
+    }
+  },
+  created(){
+    this.getLunbotu();
+  },
+  methods:{
+    getLunbotu(){
+      this.$http.get("http://www.liulongbin.top:3005/api/getlunbo").then(result=>{
+        console.log(result.body)
+        if(result.body.status===0){
+          // Toast('加载轮播图成功')
+          this.lunboList=result.body.message;
+        }else{
+          Toast('加载轮播图失败');
+        }
+      })
+    }
+  }
+}
 </script>
 
-<style>
+<style lang="scss">
+.mint-swipe{
+  height:200px;
+  .mint-swipe-item{
+  &:nth-child(1){
+    background-color: black;
+  }
+  &:nth-child(2){
+    background-color: red;
+  }
+  &:nth-child(3){
+    background-color: blue;
+  }
+  img{
+    width: 100%;
+    height: 100%;
+  }
+}
+
+}
 
 </style>
--- src/main.js
+++ src/main.js
@@ -9,13 +9,23 @@ import VueRouter from 'vue-router'
 //1.2 安装路由
 Vue.use(VueRouter)
 
+
+//2.1 导入vue-resource
+import VueResource from 'vue-resource'
+
+//2.2 安装 vue-resource
+Vue.use(VueResource)
+
+
 import 'mint-ui/lib/style.css'
 //导入mui的样式
 import './lib/mui/css/mui.min.css'
 import './lib/mui/css/icons-extra.css'
-import { Header } from 'mint-ui';
+import { Header,Swipe, SwipeItem } from 'mint-ui';
 
 Vue.component(Header.name, Header);
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
 
 
 //1.3 导入自己的router.js路由模块
